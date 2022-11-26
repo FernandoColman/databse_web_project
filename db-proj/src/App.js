e6b24e35ca42de0832b373385405bc595d8f4d1a
Fernando Colman <fernando.colman01@outlook.com>
2022-11-25 18:32:52 -0600

    Added basic User Info page and header

--- db-proj/src/App.js
+++ db-proj/src/App.js
@@ -1,16 +1,40 @@
-//import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './components/Login/Login';
+import Header from './components/Header'
 
 function App() {
+    // get variables from server
+
+    // CONTACT INFO
+    const fName = 'Fernando'
+    const lName = 'Colman'
+    const homePhone = '888-888-8888'
+    const cellPhone = '777-777-7777'
+    const email = 'fcolman@email.com'
+    const address = '123 Main Street, Houston, TX, 77494'
+
+    // TRADER INFO
+    const clientID = '123456'
+    const ethAddress = 'abcdef'
+    const goldLevel = false
+
+
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login/>} />
-        </Routes>
-      </Router>
+    <div className='main_div'>
+        <Header />
+        <h1>Hey {fName} !</h1>
+        <h3>Client ID: {clientID}</h3>
+        <h3>Ethereum Address: {ethAddress}</h3>
+        <h3>Trading Level: {goldLevel ? "Gold" : "Silver"}</h3>
+
+
+        <div className='contact_info'>
+            <h2>Your Information: </h2>
+            <p>Name: {fName + " " + lName}</p>
+            <p>Home Phone: {homePhone}</p>
+            <p>Cell Phone: {cellPhone}</p>
+            <p>Email : {email}</p>
+            <p>Address: {address}</p>
+        </div>
+
     </div>
   );
 }
